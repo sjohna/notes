88112fb0e18adf6b9b17c674362af98ba1091191
John A. Stevens <john@otherskylines.com>
2022-08-19 19:35:39 -0600

    Initial web project setup

---
+++ src/frontend/web/notes/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        devMiddleware: {
+            writeToDisk: true,
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: './src/index.html',
+                    to: 'index.html',
+                    toType: 'file',
+                }
+            ]
+        }),
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
