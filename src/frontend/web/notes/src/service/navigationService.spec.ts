e1ac400c0ab72c25755a4090fb6e19811f2f1084
John A. Stevens <john@otherskylines.com>
2023-05-13 09:00:55 -0600

    Add jest and placeholder test file for navigationService

---
+++ src/frontend/web/notes/src/service/navigationService.spec.ts
@@ -0,0 +1,9 @@
+import {describe,expect,test} from '@jest/globals';
+
+describe('navigationService', () => {
+    describe('matchRoute', () => {
+        test('should pass', () => {
+            expect(true).toBe(true);
+        });
+    });
+});
