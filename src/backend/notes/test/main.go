96c77a669e55ce21a71f76953508a72e2fb29d34
John Stevens <john.allen.stevens@fastmail.com>
2025-03-05 11:56:19 -0700

    Add tests for parameters to get document route

--- src/backend/notes/test/main.go
+++ src/backend/notes/test/main.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"gopkg.in/guregu/null.v4"
 	"net/http"
+	"notes/common"
 	"notes/repo"
 	"time"
 )
@@ -52,12 +54,16 @@ func runTests() error {
 
 	authToken = token
 
+	return loggedInTests()
+}
+
+func loggedInTests() error {
 	initialInfo, err := getGeneralInfo()
 	if err != nil {
 		return err
 	}
 
-	createdNote, err := createNote("Note created from test harness")
+	createdNote, err := createNote("First note from test harness")
 	if err != nil {
 		return err
 	}
@@ -81,7 +87,218 @@ func runTests() error {
 		fmt.Printf("PASSED\n")
 	}
 
+	err = testGetNotes()
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func testGetNotes() error {
+	// create some more notes
+	created1, err := createNote("From test harness 1")
+	if err != nil {
+		return err
+	}
+
+	created2, err := createNote("From test harness 2")
+	if err != nil {
+		return err
+	}
+
+	_, err = createNote("From test harness 3")
+	if err != nil {
+		return err
+	}
+
+	created4, err := createNote("From test harness 4")
+	if err != nil {
+		return err
+	}
+
+	created5, err := createNote("From test harness 5")
+	if err != nil {
+		return err
+	}
+
+	// test querying for notes
+	fmt.Printf("TEST: Get Notes With No Parameters: ")
+	documents, err := getDocuments(nil)
+	if err != nil {
+		return err
+	}
+
+	if !documentListContainsAll(documents, []string{"From test harness 1", "From test harness 2", "From test harness 3", "From test harness 4", "From test harness 5"}) {
+		fmt.Printf("FAILED: not all newly created documents returned!\n")
+	} else {
+		fmt.Printf("PASSED\n")
+	}
+
+	fmt.Printf("TEST: Get Only 5 Most Recent Based on Time Range: ")
+	parameters := common.NoteQueryParameters{
+		StartTime: null.TimeFrom(created1.CreatedAt),
+		EndTime:   null.TimeFrom(created5.CreatedAt),
+	}
+
+	documents, err = getDocuments(&parameters)
+	if err != nil {
+		return err
+	}
+
+	if !documentListContainsOnlyInOrder(documents, []string{"From test harness 5", "From test harness 4", "From test harness 3", "From test harness 2", "From test harness 1"}) {
+		fmt.Printf("FAILED: not all newly created documents returned!\n")
+	} else {
+		fmt.Printf("PASSED\n")
+	}
+
+	fmt.Printf("TEST: Get Middle 3 of 5 Created Based on Time Range: ")
+	parameters = common.NoteQueryParameters{
+		StartTime: null.TimeFrom(created2.CreatedAt),
+		EndTime:   null.TimeFrom(created4.CreatedAt),
+	}
+
+	documents, err = getDocuments(&parameters)
+	if err != nil {
+		return err
+	}
+
+	if !documentListContainsOnlyInOrder(documents, []string{"From test harness 4", "From test harness 3", "From test harness 2"}) {
+		fmt.Printf("FAILED: not all appropriate documents returned!\n")
+	} else {
+		fmt.Printf("PASSED\n")
+	}
+
+	fmt.Printf("TEST: Get Documents in Reverse Time Order: ")
+	parameters = common.NoteQueryParameters{
+		StartTime:     null.TimeFrom(created1.CreatedAt),
+		EndTime:       null.TimeFrom(created5.CreatedAt),
+		SortBy:        null.StringFrom("document_time"),
+		SortDirection: null.StringFrom("ascending"),
+	}
+
+	documents, err = getDocuments(&parameters)
+	if err != nil {
+		return err
+	}
+
+	if !documentListContainsOnlyInOrder(documents, []string{"From test harness 1", "From test harness 2", "From test harness 3", "From test harness 4", "From test harness 5"}) {
+		fmt.Printf("FAILED: not all appropriate documents returned!\n")
+	} else {
+		fmt.Printf("PASSED\n")
+	}
+
+	// some pagination tests
+	fmt.Printf("TEST: Get Page 1 of 5 items/page: ")
+	parameters = common.NoteQueryParameters{
+		Pagination: &common.PaginationParameters{
+			ItemsPerPage: 5,
+			PageNumber:   1,
+		},
+	}
+
+	documents, err = getDocuments(&parameters)
+	if err != nil {
+		return err
+	}
+
+	if !documentListContainsOnlyInOrder(documents, []string{"From test harness 5", "From test harness 4", "From test harness 3", "From test harness 2", "From test harness 1"}) {
+		fmt.Printf("FAILED: not all appropriate documents returned!\n")
+	} else {
+		fmt.Printf("PASSED\n")
+	}
+
+	fmt.Printf("TEST: Get Page 1 of 4 items/page: ")
+	parameters = common.NoteQueryParameters{
+		Pagination: &common.PaginationParameters{
+			ItemsPerPage: 4,
+			PageNumber:   1,
+		},
+	}
+
+	documents, err = getDocuments(&parameters)
+	if err != nil {
+		return err
+	}
+
+	if !documentListContainsOnlyInOrder(documents, []string{"From test harness 5", "From test harness 4", "From test harness 3", "From test harness 2"}) {
+		fmt.Printf("FAILED: not all appropriate documents returned!\n")
+	} else {
+		fmt.Printf("PASSED\n")
+	}
+
+	fmt.Printf("TEST: Get Page 2 of 2 items/page: ")
+	parameters = common.NoteQueryParameters{
+		Pagination: &common.PaginationParameters{
+			ItemsPerPage: 2,
+			PageNumber:   2,
+		},
+	}
+
+	documents, err = getDocuments(&parameters)
+	if err != nil {
+		return err
+	}
+
+	if !documentListContainsOnlyInOrder(documents, []string{"From test harness 3", "From test harness 2"}) {
+		fmt.Printf("FAILED: not all appropriate documents returned!\n")
+	} else {
+		fmt.Printf("PASSED\n")
+	}
+
+	fmt.Printf("TEST: Get Documents in Reverse Time Order, Page 2 of 2 items/page: ")
+	parameters = common.NoteQueryParameters{
+		StartTime:     null.TimeFrom(created1.CreatedAt),
+		EndTime:       null.TimeFrom(created5.CreatedAt),
+		SortBy:        null.StringFrom("document_time"),
+		SortDirection: null.StringFrom("ascending"),
+		Pagination: &common.PaginationParameters{
+			ItemsPerPage: 2,
+			PageNumber:   2,
+		},
+	}
+
+	documents, err = getDocuments(&parameters)
+	if err != nil {
+		return err
+	}
+
+	if !documentListContainsOnlyInOrder(documents, []string{"From test harness 3", "From test harness 4"}) {
+		fmt.Printf("FAILED: not all appropriate documents returned!\n")
+	} else {
+		fmt.Printf("PASSED\n")
+	}
+
+	return nil
+}
+
+func documentListContainsAll(documents []*repo.Document, contents []string) bool {
+	contentFound := make(map[string]bool)
+	for _, document := range documents {
+		contentFound[document.LatestVersion.Content] = true
+	}
+
+	for _, content := range contents {
+		if _, ok := contentFound[content]; !ok {
+			return false
+		}
+	}
+
+	return true
+}
+
+func documentListContainsOnlyInOrder(documents []*repo.Document, contents []string) bool {
+	if len(documents) != len(contents) {
+		return false
+	}
+
+	for i := 0; i < len(documents); i++ {
+		if documents[i].LatestVersion.Content != contents[i] {
+			return false
+		}
+	}
+
+	return true
 }
 
 func createNote(content string) (*repo.Document, error) {
@@ -128,7 +345,6 @@ func noAuthorizationHeader() {
 
 	testUnauth("POST", "/note")
 	testUnauth("POST", "/note/create")
-	testUnauth("POST", "/note/total_by_date")
 	testUnauth("POST", "/note/update_tags")
 	testUnauth("POST", "/note/update_groups")
 
@@ -147,7 +363,6 @@ func badAuthorizationHeader() {
 
 	testBadAuth("POST", "/note")
 	testBadAuth("POST", "/note/create")
-	testBadAuth("POST", "/note/total_by_date")
 	testBadAuth("POST", "/note/update_tags")
 	testBadAuth("POST", "/note/update_groups")
 
@@ -233,4 +448,21 @@ func auth(method string, url string, body interface{}) (*http.Response, error) {
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "Bearer "+authToken)
 	return client.Do(req)
+}
+
+func getDocuments(parameters *common.NoteQueryParameters) ([]*repo.Document, error) {
+	resp, err := auth("POST", "/note", parameters)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	var response struct {
+		Documents  []*repo.Document           `json:"documents"`
+		Parameters common.NoteQueryParameters `json:"parameters"`
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	return response.Documents, err
 }
